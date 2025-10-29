f1b54b9676a99de9ee937cbb588fe850334df6d2
hpe <hpe>
2025-10-29 08:23:33 -0300

    sla

--- nodejs/saudacao-mvc/controllers/clienteController.js
+++ nodejs/saudacao-mvc/controllers/clienteController.js
@@ -1,5 +1,9 @@
 module.exports = {
     cliente: (req, res) => {
     res.sendFile('cliente.html', { root: './views' });
+  },
+
+  login: (req, res) => {
+    res.sendFile('login.html', { root: './views' });
   }
 }
